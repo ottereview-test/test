ea7a3e6b8d4edab3b5eee8b8a02c6d4b05ee696c
Kangboom <jkl6147@naver.com>
2025-08-11 21:25:04 +0900

    충돌 테스트

---
+++ PrController.java
@@ -0,0 +1,15 @@
+public class PrController {
+    // This class is currently empty, but it can be used to handle pull request related requests in the future.
+    // You can add methods to handle HTTP requests related to pull requests here.   
+
+
+    // For example, you might want to add methods to create, update, delete, or retrieve pull requests.
+
+    // Example method to get all pull requests for a repository
+    // public List<PullRequest> getPullRequestsByRepo(Long repoId) {
+    //     // Logic to retrieve pull requests by repository ID
+    // }
+    // Example method to get all pull requests by an author
+    // public List<PullRequest> getPullRequestsByAuthor(Long authorId) {        
+    
+}
